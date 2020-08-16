042b14a9a531a38929061f266ad694d9b1540687
elikrm <elnazkarimpoor85@gmail.com>
2020-08-17 07:49:17 +1000

    Finished part 6

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length =200)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
 
@@ -15,11 +15,7 @@ class Question(models.Model):
     #datetime.timedelta(days=1)
 
 class Choice(models.Model):
-    question = models.ForeignKey(
-        Question, 
-        on_delete = models.CASCADE
-
-    )
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
     choice_text = models.CharField(max_length =200)
     votes = models.IntegerField(default =0)
     def __str__(self):
